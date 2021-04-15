d4f76a1285406e1090ddc974ec75d866317fdc94
Sai Mangipudi <thegeekskm101@gmail.com>
2021-04-15 18:23:46 -0500

    Added a Test Console Output

--- Assets/KrakenTest.cs
+++ Assets/KrakenTest.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class KrakenTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hopefully, this works");
     }
 
     // Update is called once per frame
